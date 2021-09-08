5b4b515b192246b01b601dc1a6474889220314b5
Alecto Irene Perez <perez.cs@pm.me>
2021-09-08 15:54:10 -0600

    Added src

--- src/hello-world.cpp
+++ src/hello-world.cpp
@@ -1,6 +1,11 @@
-#include <iostream>
 #include <fmt/core.h>
+#include <ins.hpp>
 
 int main() {
-    fmt::print("Hello, world!\n");
+    using namespace compiler;
+
+    fmt::print("test.get_OP(): {}\n", ins {0xf000'0000u}.get_OP());
+    fmt::print("test.get_A(): {}\n", ins {0b111'000'000u}.get_A());
+    fmt::print("test.get_B(): {}\n", ins {0b000'111'000u}.get_B());
+    fmt::print("test.get_C(): {}\n", ins {0b000'000'111u}.get_C());
 }
