9113a452d2f87e0454b1e9b777eed9c26dbdbfc7
jorgeypcb <jorge.loredx@gmail.com>
2021-09-08 17:15:06 -0600

    adding OP(4 and 5)

--- include/machine.hpp
+++ include/machine.hpp
@@ -67,6 +67,9 @@ class machine {
                     set_A_register(i, get_B_register(i));
                 }
                 break;
+            case 3: set_A_register(i, (get_B_register(i) + get_C_register(i))); break;
+            case 4: set_A_register(i, (get_B_register(i) * get_C_register(i))); break;
+            case 5: set_A_register(i, (get_B_register(i) / get_C_register(i))); break;
             case 8: set_B_register(i, allocate(get_C_register(i))); break;
             case 9: deallocate(get_C_register(i)); break;
         }
