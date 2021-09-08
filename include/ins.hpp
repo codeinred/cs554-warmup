9986e222f335b4139d0b93be321ebef0a1bff722
Alecto Irene Perez <perez.cs@pm.me>
2021-09-08 15:54:00 -0600

    Implemented ins.hpp

--- include/ins.hpp
+++ include/ins.hpp
@@ -1,19 +1,32 @@
 #pragma once
+#include <cstdint>
 
 namespace compiler {
-using uint = unsigned int;
+using uint = uint32_t;
 
+template <int start, int end>
+constexpr uint get_section_of_bits(uint input) {
+    uint num_to_get = end - start + 1;
+    uint mask = (1 << num_to_get) - 1;
+    return (input >> start) & mask;
+}
 struct ins {
     uint code = 0;
 
-    uint get_OP() {
-        return 0;
+    constexpr uint get_OP() const {
+        return get_section_of_bits<28, 31>(code);
     }
-    uint get_A() {
-        return 0;
+    constexpr uint get_A() const {
+        return get_section_of_bits<6, 8>(code);
     }
-    uint get_C() {
-        return 0;
+    constexpr uint get_B() const {
+        return get_section_of_bits<3, 5>(code);
+    }
+    constexpr uint get_C() const {
+        return get_section_of_bits<0, 2>(code);
     }
 };
+
+
+static_assert(ins{ 0xf0000000u}.get_OP() == 0xf, "get_OP() not working");
 }
