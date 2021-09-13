cc843d1cfa07b652bf6e9a2eb65fbf2b93ac77d9
Alecto Irene Perez <perez.cs@pm.me>
2021-09-13 11:51:42 -0600

    Looks for g++-11; uses g++ if g++-11 can't be found

--- build.sh
+++ build.sh
@@ -9,6 +9,12 @@ project_root="$(dirname $0)"
 mkdir -p "$project_root/build"
 cd "$project_root/build"
 
+if which g++-11 2> /dev/null; then
+    compiler="g++-11"
+else
+    compiler="g++"
+fi
+
 # build project in $project_root/build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=g++-11 "${@}"
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="${compiler}" "${@}"
 cmake --build . --config Release
